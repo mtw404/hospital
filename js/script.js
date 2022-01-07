dd807eae6bfe9d183cc33b95d3071e8e08eea6db
mtw404 <mirza.tahmid404@gmail.com>
2022-01-07 22:27:10 +0600

    working

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+
+
+
+  $('.venobox').venobox({
+    spinner: 'cube-grid',
+    spinColor: '##6f00ff',
+    arrowsColor: '#e9b68d',
+    bgcolor: 'white',
+    border: '6px',
+    overlayColor: 'rgba(243, 27, 27, 0.623)',
+});
+
+
+
+
+    
+});
